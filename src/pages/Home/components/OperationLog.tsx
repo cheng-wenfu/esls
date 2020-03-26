1062f4bd77f04f1f7575f6812333b57dc1943967
cwf <chengwenfu00@gmail.com>
2020-03-26 20:28:01 +0800

    修改文件格式tsx

---
+++ src/pages/Home/components/OperationLog.tsx
@@ -0,0 +1,42 @@
+import React from 'react';
+import { Table } from 'antd';
+
+export interface LogDataType {
+  userName: string;
+  operation: string;
+  ipAddress: string;
+  time: string;
+}
+
+interface OperationLogProps {
+  operationLog: LogDataType[];
+}
+
+const OperationLog: React.FC<OperationLogProps> = ({ operationLog }) => {
+  const columns = [
+    {
+      title: '用户名',
+      dataIndex: 'userName',
+      key: 'userName',
+    },
+    {
+      title: '操作',
+      ataIndex: 'operation',
+      key: 'operation',
+    },
+    {
+      title: 'IP',
+      dataIndex: 'ipAddress',
+      key: 'ipAddress',
+    },
+    {
+      title: '执行时间',
+      dataIndex: 'time',
+      key: 'time',
+    },
+  ];
+
+  return <Table dataSource={operationLog} columns={columns} />;
+};
+
+export default OperationLog;
