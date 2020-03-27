b0f98904c1f8b14683c005dad96e3556bda4fda0
cwf <chengwenfu00@gmail.com>
2020-03-27 22:48:34 +0800

    商品管理页面

---
+++ src/pages/GoodManage/GoodManage.tsx
@@ -0,0 +1,125 @@
+import React, { Component } from 'react';
+import { PageHeaderWrapper } from '@ant-design/pro-layout';
+import { Row } from 'antd';
+import { connect } from 'dva';
+import { Dispatch, AnyAction } from 'redux';
+
+import GoodsTable from './components/GoodsTable';
+import TagsTable from './components/tagsTable';
+
+import { ConnectState, GoodManageModelState } from '@/models/connect';
+import { GoodsDataType, TagsDataType } from '@/models/goodManage';
+
+interface GoodManageProps {
+  dispatch: Dispatch<AnyAction>;
+  pageData: GoodManageModelState;
+}
+
+interface GoodManageState {
+  goodsPage: number;
+  tagsPage: number;
+}
+
+class GoodManage extends Component<GoodManageProps, GoodManageState> {
+  state: GoodManageState = {
+    goodsPage: 0,
+    tagsPage: 0,
+  };
+
+  componentDidMount() {
+    const { dispatch } = this.props;
+    dispatch({
+      type: 'goodManage/fetchGoodsData',
+      payload: this.state.goodsPage,
+    });
+    dispatch({
+      type: 'goodManage/fetchTagsData',
+      payload: this.state.tagsPage,
+    });
+  }
+
+  /**
+   * 获取商品表格所需的字段
+   */
+
+  getGoodsTableData(goodsData: Array<GoodsDataType>): Array<GoodsDataType> {
+    const goodsTableData = [];
+    for (let i = 0; i < goodsData.length; i++) {
+      let {
+        name, //商品名称
+        price, //原价
+        stock, //库存
+        promotePrice, //促销价格
+        isPromote, //是否促销，0否 1是
+        shopNumber, //店铺
+        shelfNumber, //货号
+        status, //绑定状态 1代表绑定
+        waitUpdate,
+      } = goodsData[i];
+      goodsTableData.push({
+        name, //商品名称
+        price, //原价
+        stock, //库存
+        promotePrice, //促销价格
+        isPromote, //是否促销，0否 1是
+        shopNumber, //店铺
+        shelfNumber, //货号
+        status, //绑定状态 1代表绑定
+        waitUpdate,
+      });
+    }
+    return goodsTableData;
+  }
+
+  /**
+   * 获取标签表格所需的字段
+   */
+  getTagsTableData(tagsData: Array<TagsDataType>): Array<TagsDataType> {
+    const tagsTableData = [];
+    for (let i = 0; i < tagsData.length; i++) {
+      let {
+        barCode, //价签条码
+        screenType, //价签屏幕类型
+        routerBarCodeAndChannelId, // AP/信道
+        power, //电量
+        apRssi,
+        tagRssi,
+        isWorking, //通讯状态， 1 可用
+        forbidState, //启用状态，1 启用
+        waitUpdate,
+      } = tagsData[i];
+      tagsTableData.push({
+        barCode, //价签条码
+        screenType, //价签屏幕类型
+        routerBarCodeAndChannelId, // AP/信道
+        power, //电量
+        apRssi,
+        tagRssi,
+        isWorking, //通讯状态， 1 可用
+        forbidState, //启用状态，1 启用
+        waitUpdate,
+      });
+    }
+    return tagsTableData;
+  }
+
+  render() {
+    const { goodsData, tagsData } = this.props.pageData;
+    const goodsTableData = this.getGoodsTableData(goodsData);
+    const tagsTableData = this.getTagsTableData(tagsData);
+    return (
+      <PageHeaderWrapper>
+        <Row>
+          <GoodsTable goodsData={goodsTableData} />
+        </Row>
+        <Row>
+          <TagsTable tagsData={tagsTableData} />
+        </Row>
+      </PageHeaderWrapper>
+    );
+  }
+}
+
+export default connect(({ goodManage }: ConnectState) => ({
+  pageData: goodManage,
+}))(GoodManage);
