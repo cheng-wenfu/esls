77adea2f4d03c0e851c5ee171a3fb15a571bdfe1
cwf <chengwenfu00@gmail.com>
2020-03-27 22:45:54 +0800

    初始化首页的数据

--- src/models/home.ts
+++ src/models/home.ts
@@ -4,19 +4,19 @@ import { Effect } from 'dva';
 import { getCommonIndex, getOperationLog } from '@/services/home';
 
 export interface commonIndexType {
-  goodSize?: number;
-  tagSize?: number;
-  routerSize?: number;
-  styleSzie?: number;
-  shopSize?: number;
-  userSize?: number;
-  normalTagSize?: number;
-  noBindTagSize?: number;
-  noIsWorkingTagSize?: number;
-  forbiddenTagSize?: number;
-  normalRouterSize?: number;
-  forbiddenRouterSize?: number;
-  noIsWorkingRouterSize?: number;
+  goodSize: number;
+  tagSize: number;
+  routerSize: number;
+  styleSzie: number;
+  shopSize: number;
+  userSize: number;
+  normalTagSize: number;
+  noBindTagSize: number;
+  noIsWorkingTagSize: number;
+  forbiddenTagSize: number;
+  normalRouterSize: number;
+  forbiddenRouterSize: number;
+  noIsWorkingRouterSize: number;
 }
 
 export interface LogType {
@@ -57,8 +57,30 @@ const Model: HomeModelType = {
   namespace: 'home',
 
   state: {
-    commonIndex: {},
-    operationLog: [],
+    commonIndex: {
+      //默认值
+      goodSize: 0,
+      tagSize: 0,
+      routerSize: 0,
+      styleSzie: 0,
+      shopSize: 0,
+      userSize: 0,
+      normalTagSize: 0,
+      noBindTagSize: 0,
+      noIsWorkingTagSize: 0,
+      forbiddenTagSize: 0,
+      normalRouterSize: 0,
+      forbiddenRouterSize: 0,
+      noIsWorkingRouterSize: 0,
+    },
+    operationLog: [
+      {
+        logDescription: '',
+        userName: '',
+        ip: '',
+        createTime: 0,
+      },
+    ],
   },
 
   effects: {
