3d7ebb65aee24ef8570699d170df52015d72edc7
cwf <chengwenfu00@gmail.com>
2020-03-27 22:54:10 +0800

    商品管理服务器请求

---
+++ src/services/goodManage.ts
@@ -0,0 +1,15 @@
+import request from '@/utils/request';
+
+export async function getGoodsData(page: number) {
+  const url = `/api/goods/?page=${page}&count=7`;
+  return request(url, {
+    method: 'GET',
+  });
+}
+
+export async function getTagsData(page: number) {
+  const url = `/api//tags/?page=${page}&count=3`;
+  return request(url, {
+    method: 'GET',
+  });
+}
